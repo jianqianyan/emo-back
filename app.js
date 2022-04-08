35d892aa5d7cf8e57be075b91ba9c4c57ccc48eb
qianyan <836938311@qq.com>
2022-04-08 23:09:39 +0800

    增加验证码接口 登录增加验证码登录

--- app.js
+++ app.js
@@ -17,31 +17,64 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+// 使用express-session 来存放数据到session中
+var cookieSession = require('cookie-session')
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
+// 设置跨域头
+app.all("*", (req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    req.headers.origin || req.headers.referer || "*"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("X-Powered-By", "Express");
+  if (req.method == "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // 不需要登录的路由
 app.use('/', indexRouter);
-app.use('/login' , loginRouter);
+app.use('/login', loginRouter);
 
 // token 解析
 const parseJwt = require('express-jwt');
 const SECRET_KEY = 'jianqianyan';
 app.use(
   parseJwt({
-      secret: SECRET_KEY,
-      algorithms: ['HS256'], // 使用何种加密算法解析
+    secret: SECRET_KEY,
+    algorithms: ['HS256'], // 使用何种加密算法解析
   })
-    .unless({ path: [] })
+  .unless({
+    path: []
+  })
 )
 
 // 需要登录的路由
 app.use('/users', usersRouter);
-app.use('/hello' , helloRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
@@ -50,7 +83,7 @@ const errorhandler = require('./middleware/errorhandler')
 app.use(errorhandler)
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -59,5 +92,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
--- routes/login.js
+++ routes/login.js
@@ -2,47 +2,62 @@ var express = require('express');
 var router = express.Router();
 var db = require('../untli/db/db')
 var setToken = require('../untli/token/token')
+const svgCaptcha = require("svg-captcha")
+
+// 验证码
+router.get('/img_code', async (req, res) => {
+  const captcha = svgCaptcha.create({
+    noise: 3,
+  });
+  req.session.img_code = captcha.text;
+  res.type('svg');
+  res.send(captcha.data);
+});
 
 // 登录验证
-// 登录成功返回用户id,账号不存在返回-1,密码错误返回-2,登录失败返回-3
+// 登录成功返回用户id,账号不存在返回-1,密码错误返回-2,登录失败返回-3,验证码错误返回-4
 router.get('/', async function (req, res, next) {
   let message = req.query;
-  let phone = message.phone , password = message.password;
-  let user = {} , retunmes = {};
-  // 尝试查找是否有users
-  try {
-    user = await db.find("users", {
-      "phone": phone
-    });
-  } catch (err) {
-    console.log(err.message);
-    retunmes.data = -3;
-  }
-  // 存在用户
-  let token;
-  if (user.length == 0) {
-    retunmes.data = -1;
+  let phone = message.phone,
+    password = message.password,
+    img_code = message.img_code;
+  let user = {},
+    retunmes = {};
+  retunmes.statue = 200;
+  console.log(img_code);
+  if (img_code != req.session.img_code) {
+    // 验证验证码是否正确
+    retunmes.data = -4;
   } else {
-    if (String(user[0].password) == String(password)) {
-      // 登录成功
-      retunmes.data = 1;
-      retunmes.userid = user[0].id;
-
-      // 设置cookie
-      // res.cookie('user_id', retunmes.userid, {maxAge: 600000});
-
-      // 设置token
-      token = setToken(retunmes);
-
+    // 尝试查找是否有users
+    try {
+      user = await db.find("users", {
+        "phone": phone
+      });
+    } catch (err) {
+      console.log(err.message);
+      retunmes.data = -3;
+    }
+    // 存在用户
+    let token;
+    if (user.length == 0) {
+      retunmes.data = -1;
     } else {
-      retunmes.data = -2;
+      if (String(user[0].password) == String(password)) {
+        // 登录成功
+        retunmes.data = 1;
+        retunmes.userid = user[0].id;
+
+        // 设置token
+        token = setToken(retunmes);
+        retunmes.token = token;
+      } else {
+        retunmes.data = -2;
+      }
     }
   }
-  res.send({
-    status: 200,
-    message: retunmes,
-    token
-  });
+
+  res.send(retunmes);
 })
 
 // 注册
@@ -54,18 +69,18 @@ router.post('/register', async function (req, res, next) {
   });
   if (data.length == 0) {
     let data = await db.add("users", message);
-    
+
     if (data == "-1") {
       retunmes.message = "注册失败";
     } else {
       retunmes.message = "注册成功";
     }
-  }
-  else{
+  } else {
     retunmes.message = "该电话号码已被注册";
   }
 
   retunmes.status = 200;
   res.send(retunmes);
 })
+
 module.exports = router;
