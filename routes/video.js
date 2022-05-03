6e43cebb9a61201a72568df91f5476fb81bbb747
qianyan <836938311@qq.com>
2022-05-03 21:47:03 +0800

    优化视频播放接口

--- routes/video.js
+++ routes/video.js
@@ -38,53 +38,59 @@ router.get('/getmessage', async function (req, res, next) {
 
 // 流式传输视频
 router.get('/', async function (req, res, next) {
-    // 验证是否有该视频
+    let path = req.query.path;
+    let filePath = './assets/video/' + path;
+    try {
+        let stat = fs.statSync(filePath);
+        let fileSize = stat.size;
+        let range = req.headers.range;
+        if (range) {
+            // 有range头才使用206状态码            
+            let parts = range.replace(/bytes=/, "").split("-");
+            let start = parseInt(parts[0], 10);
+            let end = parts[1] ? parseInt(parts[1], 10) : start + 999999;
 
-    let path = './assets/video/';
-    path = path + req.query.video_path;
-    let stat = fs.statSync(path);
-    let fileSize = stat.size;
-    let range = req.headers.range;
-    // fileSize 3332038
-    if (range) {
-        //有range头才使用206状态码
-        // console.log("aaa");
-        let parts = range.replace(/bytes=/, "").split("-");
-        let start = parseInt(parts[0], 10);
-        let end = parts[1] ? parseInt(parts[1], 10) : start + 999999;
-        if(start == 0){
-            let video = await db.find("videos" , {"path" : req.query.video_path});
-            if(video.length > 0){
-                videoCount.videoCount(video[0].id);
+            if (start == 0) {
+                let video;
+                try {
+                    video = await db.find("videos", {
+                        "path": path
+                    });
+                } catch (err) {
+                    console.log(err);
+                }
+                // 这里可以加上对于已经被删除的视频的播放限制
+                if (video.length > 0) {
+                    videoCount.videoCount(video[0].id);
+                    
+                } else {}
             }
+            // end 在最后取值为 fileSize - 1 
+            end = end > fileSize - 1 ? fileSize - 1 : end;
+
+            let chunksize = (end - start) + 1;
+            let file = fs.createReadStream(filePath, {
+                start,
+                end
+            });
+            let head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            let head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(filePath).pipe(res);
         }
-        // end 在最后取值为 fileSize - 1 
-        end = end > fileSize - 1 ? fileSize - 1 : end;
-
-        let chunksize = (end - start) + 1;
-        let file = fs.createReadStream(path, {
-            start,
-            end
-        });
-        let head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-        // 计数器
-        videoCount.videoCount(video[0].id);
-
-        let head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
+    } catch (err) {
+        console.log(err);
     }
-
-});
+})
 module.exports = router;
